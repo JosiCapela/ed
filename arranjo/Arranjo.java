432f137714491a9c395361a43fa845c3628c2401
Josiane Capela <89423264+JosiCapela@users.noreply.github.com>
2021-08-27 20:11:54 -0300

    Update Arranjo.java

--- arranjo/Arranjo.java
+++ arranjo/Arranjo.java
@@ -23,6 +23,80 @@ public class Arranjo<T> {
             System.out.print(this.Data[i]+" ");
         }
         System.out.println("]");
+
+#include 
+
+struct Fila {
+
+	int capacidade;
+	float "a","b","c","d","e";
+	int primeiro;
+	int ultimo;
+	int nItens; 
+
+};
+
+void criarFila( struct Fila *f, int c ) { 
+
+	f->capacidade = c;
+	f->dados = (float*) malloc (f->capacidade * sizeof(float));
+	f->primeiro = 0;
+	f->ultimo = -1;
+	f->nItens = 0; 
+
+}
+
+void inserir(struct Fila *f, int v) {
+
+	if(f->ultimo == f->capacidade-1)
+		f->ultimo = -1;
+
+	f->ultimo++;
+	f->dados[f->ultimo] = v; // incrementa ultimo e insere
+	f->nItens++; // mais um item inserido
+
+}
+
+int remover( struct Fila *f ) { // pega o item do comeco da fila
+
+	int temp = f->dados[f->primeiro++]; // pega o valor e incrementa o primeiro
+
+	if(f->primeiro == f->capacidade)
+		f->primeiro = 0;
+
+	f->nItens--;  // um item retirado
+	return temp;
+
+}
+
+int estaVazia( struct Fila *f ) { // retorna verdadeiro se a fila esta vazia
+
+	return (f->nItens==0);
+
+}
+
+int estaCheia( struct Fila *f ) { // retorna verdadeiro se a fila esta cheia
+
+	return (f->nItens == f->capacidade);
+}
+
+void mostrarFila(struct Fila *f){
+
+	int cont, i;
+
+	for ( cont=0, i= f->primeiro; cont < f->nItens; cont++){
+
+		printf("%.2f\t",f->dados[i++]);
+
+		if (i == f->capacidade)
+			i=0;
+
+	}
+	printf("\n\n");
+
+}
+
+
     }
 
 }
